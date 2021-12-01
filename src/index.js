d1d6bbae9b08bcb9a1aa33f5cd70047e5496ed4c
Rakesh <rakeshmacho2016@gmail.com>
2021-12-01 21:01:59 +0530

    FINAL React + Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import ThunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(ThunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
